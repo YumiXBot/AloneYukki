becd8b2a877d6fd0844ba0413cd49f6cd9960546
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2023-10-22 16:11:31 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/YumiXBot/AloneYukki/edit",
+    "https://github.com/YumiXBot/AloneYukki",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
